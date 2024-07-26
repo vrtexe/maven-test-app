a0e64381d0a9c7e792202fa3dad6cc3aeaa059f5
vrtexe <traykovski5@gmail.com>
2024-07-26 09:43:08 +0200

    initial commit

---
+++ src/main/java/mk/vt/app/maventestapp/MavenTestAppApplication.java
@@ -0,0 +1,13 @@
+package mk.vt.app.maventestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/vt/app/maventestapp/MavenTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.vt.app.maventestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
