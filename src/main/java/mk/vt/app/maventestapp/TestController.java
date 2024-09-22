f2cbc286c6e832bb3b9b605ee63374e61366b76f
vrtexe <traykovski5@gmail.com>
2024-09-22 15:31:09 +0200

    Add a test controller

---
+++ src/main/java/mk/vt/app/maventestapp/TestController.java
@@ -0,0 +1,16 @@
+package mk.vt.app.maventestapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+
+    @GetMapping
+    String test() {
+        return "Maven test app";
+    }
+}
